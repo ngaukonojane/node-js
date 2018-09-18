a23ff277c0ab7fbc1166fc354ad56c10a314ee3c
Jane <ngaukonojane@gmail.com>
2018-09-18 15:17:31 +0300

    first commit

---
+++ vue-template/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h3>{{title}}</h3>
+    <form>
+    <textarea placeholder="Add new task"></textarea>
+    <button type="button">Add Task</button>
+    </form>
+    <ul>
+    <li> v-for="task in tasks">{{ task }}</li>
+    </ul>      
+
+  
+    </div>
+    </template>
+
+    <script>
+    export default{
+      data:function(){
+        return{
+          title:'my task manager',          
+          task:{
+            "Task one",
+            "Task two", 
+            "Task three",
+            "Task four",
+          }
+        }
+      }
+    }
+
+ 
+
+</script>
+
